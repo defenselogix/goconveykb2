723bab2461ecb25cdd1b1712ee2ffa17c6516c04
defenselogix <scott@defenselogix.com>
2026-02-23 16:43:55 -0800

    Rename 'EONS Knowledge Base' to 'Convey Knowledge Base'

--- src/App.jsx
+++ src/App.jsx
@@ -125,7 +125,7 @@ function AppContent() {
             <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20" />
             <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z" />
           </svg>
-          <span>EONS Knowledge Base</span>
+          <span>Convey Knowledge Base</span>
         </div>
         <div className="header-search">
           <svg className="header-search-icon" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
